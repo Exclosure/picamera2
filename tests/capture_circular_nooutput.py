99ced4e41ba6bcb56c38960193a495afe3beaba0
meawoppl <meawoppl@gmail.com>
2022-12-24 13:22:29 -0800

    Dead encoder

--- tests/capture_circular_nooutput.py
+++ tests/capture_circular_nooutput.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-import sys
 import time
 
 from picamera2 import Picamera2
-from picamera2.encoders import MJPEGEncoder
+from picamera2.encoders.jpeg_encoder import JpegEncoder
 from picamera2.outputs import CircularOutput
 
 picam2 = Picamera2()
@@ -13,7 +12,7 @@ micro = int((1 / fps) * 1000000)
 vconfig = picam2.create_video_configuration()
 vconfig["controls"]["FrameDurationLimits"] = (micro, micro)
 picam2.configure(vconfig)
-encoder = MJPEGEncoder()
+encoder = JpegEncoder()
 output = CircularOutput(buffersize=int(fps * (dur + 0.2)), outputtofile=False)
 output.fileoutput = "file.mjpeg"
 picam2.start_recording(encoder, output)
