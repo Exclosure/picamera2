3ca566766badeccc7e8b3f404a69ceb48e56ac6e
meawoppl <meawoppl@gmail.com>
2022-12-27 13:33:23 -0800

    jc

--- tests/still_during_video.py
+++ tests/still_during_video.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from picamera2 import Picamera2
-
+from picamera2.request import CompletedRequest
 # Encode a VGA stream, and capture a higher resolution still image half way through.
 
 camera = Picamera2()
@@ -9,9 +9,13 @@ main_stream = {"size": half_resolution}
 lores_stream = {"size": (640, 480)}
 video_config = camera.create_video_configuration(main_stream, lores_stream)
 camera.configure(video_config)
+camera.start()
 
 # It's better to capture the still in this thread, not in the one driving the camera.
-request = camera.capture_request()
+request: CompletedRequest = camera.capture_request()
 request.save("main", "test.jpg")
 request.release()
 print("Still image captured!")
+
+camera.stop()
+camera.close()
