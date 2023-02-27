adf84e5ad75d504d2d416a4dd89e7cf24e9da438
meawoppl <meawoppl@gmail.com>
2023-02-26 22:08:56 -0800

    make the fake more useful

--- scicamera/fake.py
+++ scicamera/fake.py
@@ -1,24 +1,26 @@
 from threading import Event, Thread
 from typing import Any, Dict
 
+import libcamera
 import numpy as np
 
 from scicamera.actions import RequestMachinery
+from scicamera.configuration import CameraConfig, StreamConfig
 from scicamera.request import CompletedRequest
 
 
-def make_fake_image(shape, dtype=np.uint8):
-    img = np.zeros(shape + (3,), dtype=dtype)
+def make_fake_image(shape: tuple):
+    img = np.zeros(shape + (3,), dtype=np.uint8)
     w, h = shape
-    img[0 : w / 3, :, 0] = 255
-    img[w / 3 : 2 * w / 3, :, 1] = 255
-    img[2 * w / 3 :, :, 2] = 255
+    img[:, 0 : w // 3, 0] = 255
+    img[:, w // 3 : 2 * w // 3, 1] = 255
+    img[:, 2 * w // 3 :, 2] = 255
     return img
 
 
 class FakeCompletedRequest(CompletedRequest):
-    def __init__(self, fake_size: tuple):
-        self._fake_size = fake_size
+    def __init__(self, config: CameraConfig):
+        self._config = config
 
     def acquire(self):
         pass
@@ -28,11 +30,12 @@ class FakeCompletedRequest(CompletedRequest):
 
     def get_config(self, name: str) -> Dict[str, Any]:
         """Fetch the configuration for the named stream."""
-        return {}
+        return self._config
 
     def get_buffer(self, name: str) -> np.ndarray:
         """Make a 1d numpy array from the named stream's buffer."""
-        return make_fake_image(self._fake_size).flatten()
+        size = self._config.get_config(name).size
+        return make_fake_image(size).flatten()
 
     def get_metadata(self) -> Dict[str, Any]:
         """Fetch the metadata corresponding to this completed request."""
@@ -42,18 +45,32 @@ class FakeCompletedRequest(CompletedRequest):
 class FakeCamera(RequestMachinery):
     def __init__(self) -> None:
         super().__init__()
-        self._t = Thread(target=lambda x: None, daemon=True)
+        self._t = Thread(target=lambda: None, daemon=True)
         self._t.start()
         self._t.join()
         self._abort = Event()
 
+        self.config = CameraConfig(
+            self,
+            "still",
+            1,
+            controls={},
+            transform=libcamera.Transform(),
+            color_space=libcamera.ColorSpace.Sycc(),
+            main=StreamConfig((320, 240), format="BGR888", stride=320 * 3),
+        )
+
     def _run(self):
         while not self._abort.wait(0.1):
-            request = FakeCompletedRequest((640, 480))
+            request = FakeCompletedRequest(self.config)
             self.add_completed_request(request)
+            self.process_requests()
+
+    def configure(self, config: CameraConfig) -> None:
+        self.config = config
 
     def start(self) -> None:
-        self._t = Thread(target=self._run)
+        self._t = Thread(target=self._run, daemon=True)
         self._abort.clear()
         self._t.start()
 
--- tests/test_fake_camera.py
+++ tests/test_fake_camera.py
@@ -1,9 +1,69 @@
+import time
+
+import numpy as np
+import pytest
+from PIL import Image
+
 from scicamera.fake import FakeCamera
 from scicamera.testing import mature_after_frames_or_timeout
 
-camera = FakeCamera()
-camera.start()
 
-mature_after_frames_or_timeout(camera, 10, 1)
-camera.stop()
-camera.close()
+@pytest.fixture
+def camera():
+    camera = FakeCamera()
+    camera.start()
+    yield camera
+    camera.stop()
+    camera.close()
+
+
+def test_fake_camera_init():
+    FakeCamera()
+
+
+def test_fake_camera_run_internals():
+    camera = FakeCamera()
+    camera.start()
+
+    mature_after_frames_or_timeout(camera, 10, 1)
+
+    camera.stop()
+    camera.close()
+
+
+def test_fake_camera_makes_images():
+    camera = FakeCamera()
+
+    capt = camera.capture_array()
+    time.sleep(0.1)
+    assert not capt.done()
+
+    camera.start()
+
+    camera.capture_array().result(timeout=0.5)
+    camera.capture_metadata().result()
+    a = [frame.result for frame in camera.capture_serial_frames(3)]
+    camera.capture_buffer()
+
+    camera.stop()
+    camera.close()
+
+
+def test_fake_metadata(camera: FakeCamera):
+    metadata = camera.capture_metadata().result(timeout=0.2)
+    assert isinstance(metadata, dict)
+
+
+def test_fake_array(camera: FakeCamera):
+    array = camera.capture_array().result(timeout=0.2)
+    assert isinstance(array, np.ndarray)
+
+
+def test_fake_buffer(camera: FakeCamera):
+    buffer = camera.capture_buffer().result(timeout=0.2)
+    assert isinstance(buffer, np.ndarray)
+
+
+def test_fake_image(camera: FakeCamera):
+    image = camera.capture_image().result(timeout=0.2)
+    assert isinstance(image, Image.Image)
