37fa264e184282a8755d296846763257f5ff142f
meawoppl <meawoppl@gmail.com>
2023-02-27 18:27:26 -0800

    Lint

--- scicamera/__init__.py
+++ scicamera/__init__.py
@@ -3,10 +3,10 @@ import importlib.metadata
 import libcamera
 
 from scicamera.camera import Camera
-from scicamera.info import CameraInfo
 from scicamera.configuration import CameraConfig, StreamConfig
 from scicamera.controls import Controls
 from scicamera.fake import FakeCamera
+from scicamera.info import CameraInfo
 from scicamera.lc_helpers import libcamera_color_spaces_eq, libcamera_transforms_eq
 
 # NOTE(meawoppl) - ugleeee monkey patch. Kill the below VV
@@ -20,8 +20,7 @@ libcamera.ColorSpace.__eq__ = libcamera_color_spaces_eq
 __all__ = [
     "Camera",
     "CameraConfig",
-    "CameraInfo"
-    "CompletedRequest",
+    "CameraInfo" "CompletedRequest",
     "Controls",
     "FakeCamera",
     "StreamConfig",
