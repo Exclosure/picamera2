045df76df6e73a300b50836084bfd1962403dfa3
meawoppl <meawoppl@gmail.com>
2023-03-03 14:19:02 -0800

    Better testing of timeout helper

--- tests/test_timeout_helper.py
+++ tests/test_timeout_helper.py
@@ -9,10 +9,9 @@ from scicamera.testing import mature_after_frames_or_timeout
 @pytest.mark.parametrize("CameraClass", [Camera, FakeCamera])
 def test_timeout_helper(CameraClass: Type[Camera]):
     camera = CameraClass()
-    try:
-        mature_after_frames_or_timeout(camera, 2, 0.2)
-    except TimeoutError:
-        print("Timed out! (expected)")
+    with pytest.raises(TimeoutError):
+        mature_after_frames_or_timeout(camera, 2, 0.1)
 
     camera.start()
     mature_after_frames_or_timeout(camera)
+    camera.stop()
