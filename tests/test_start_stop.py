929317dd1ebdc588458d9a4739fa2e366adcd711
meawoppl <meawoppl@gmail.com>
2023-03-10 08:50:32 -0800

    GC?

--- tests/test_start_stop.py
+++ tests/test_start_stop.py
@@ -26,11 +26,16 @@ def test_different_name_start_stop_runloop(CameraClass: Type[Camera]):
 
 @pytest.mark.parametrize("CameraClass", [Camera, FakeCamera])
 def test_start_stop(CameraClass: Type[Camera]):
+    import gc
     with CameraClass() as camera:
         for cycle in range(3):
             _log.warning("Starting camera %s", cycle)
             camera.start()
             mature_after_frames_or_timeout(camera)
+            time.sleep(0.1)
+            gc.collect()
+
             camera.stop()
             _log.warning("Stopped camera %s", cycle)
             time.sleep(0.1)
+            gc.collect()
