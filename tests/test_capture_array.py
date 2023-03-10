04ec6f3bf7c8992f6242e1f8edcc2c7404eb0993
meawoppl <meawoppl@gmail.com>
2023-03-10 15:52:44 -0800

    Add back tests

--- tests/test_capture_array.py
+++ tests/test_capture_array.py
@@ -13,9 +13,23 @@ def test_capture_file_encodings(CameraClass: Type[Camera]):
     with CameraClass() as camera:
         preview_config = CameraConfig.for_preview(camera, main={"size": (800, 600)})
         camera.configure(preview_config)
-
         camera.start()
         mature_after_frames_or_timeout(camera)
         array = camera.capture_array().result()
         assert isinstance(array, np.ndarray)
         camera.stop()
+
+
+@pytest.mark.parametrize("CameraClass", [Camera, FakeCamera])
+def test_capture_array_mode_change(CameraClass: Type[Camera]):
+    with CameraClass() as camera:
+        camera.start_preview()
+
+        preview_config = CameraConfig.for_preview(camera)
+        still_config = CameraConfig.for_still(camera)
+        camera.configure(preview_config)
+
+        camera.start()
+        camera.discard_frames(2)
+        array = camera.capture_array(config=still_config).result()
+        assert array.shape == camera.sensor_resolution[::-1] + (3,)
