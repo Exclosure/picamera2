fb54f469b6ed93dba6c8d3eb357d7aeee24c1e79
meawoppl <meawoppl@gmail.com>
2023-03-10 16:42:09 -0800

    Lint

--- tests/test_start_stop.py
+++ tests/test_start_stop.py
@@ -25,6 +25,7 @@ def test_different_name_start_stop_runloop(CameraClass: Type[Camera]):
         mature_after_frames_or_timeout(camera)
         camera.stop()
 
+
 @pytest.mark.skipif(I_WANNA_BRICK_THE_CAMERA, reason="This test bricks the PI")
 @pytest.mark.parametrize("CameraClass", [Camera, FakeCamera])
 def test_start_stop(CameraClass: Type[Camera]):
