cc35a90fd479167a07b892c0b10e8aa1d72e0efa
meawoppl <meawoppl@gmail.com>
2023-07-06 21:05:42 -0700

    Get the order right

--- tests/test_raw.py
+++ tests/test_raw.py
@@ -27,6 +27,7 @@ def test_capture_raw():
         preview_config = CameraConfig.for_preview(camera)
         preview_config.enable_raw()
         print(preview_config)
+        width, height = preview_config.raw.size
 
         camera.configure(preview_config)
         assert camera.camera_configuration().get_stream_config("raw") is not None
@@ -37,58 +38,9 @@ def test_capture_raw():
         print(raw.shape)
 
         if camera.info.model == "imx477":
-            assert raw.shape == preview_config.raw.size
             assert camera.sensor_format == "SBGGR12_CSI2P"
 
         if camera.info.model == "imx219":
-            assert raw.shape == preview_config.raw.size
             assert camera.sensor_format == "SBGGR10_CSI2P"
 
-        assert raw.shape == camera.sensor_format
-
-
-def test_raw_stacking():
-    exposure_time = 60000
-    num_frames = 6
-
-    # Configure an unpacked raw format as these are easier to add.
-    with Camera() as camera:
-        _skip_if_no_raw(camera)
-
-        raw_format = SensorFormat(camera.sensor_format)
-        raw_format.packing = None
-        config = CameraConfig.for_still(
-            camera, raw={"format": raw_format.format}, buffer_count=2
-        )
-        camera.configure(config)
-        images = []
-        camera.set_controls(
-            {"ExposureTime": exposure_time // num_frames, "AnalogueGain": 1.0}
-        )
-        camera.start()
-
-        # The raw images can be added directly using 2-byte pixels.
-        for i in range(num_frames):
-            _log.info("Captureing frame %s", i + 1)
-            images.append(camera.capture_array("raw").result().view(np.uint16))
-        metadata = camera.capture_metadata().result()
-
-        accumulated = images.pop(0).astype(int)
-        for image in images:
-            accumulated += image
-
-        # Fix the black level, and convert back to uint8 form for saving as a DNG.
-        black_level = metadata["SensorBlackLevels"][0] / 2 ** (
-            16 - raw_format.bit_depth
-        )
-        accumulated -= (num_frames - 1) * int(black_level)
-        accumulated = accumulated.clip(0, 2**raw_format.bit_depth - 1).astype(
-            np.uint16
-        )
-        accumulated = accumulated.view(np.uint8)
-        metadata["ExposureTime"] = exposure_time
-
-        with tempfile.TemporaryDirectory() as tmpdir:
-            path = os.path.join(tmpdir, "accumulated.jpg")
-            Image.fromarray(accumulated).save(path)
-            assert os.path.isfile(path)
+        assert raw.shape == height, width
