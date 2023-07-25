c760d0c73071747698b8ebd7d4fe03a425ab225f
meawoppl <meawoppl@gmail.com>
2023-07-25 12:22:35 -0700

    More migrations

--- tests/test_tuning_file.py
+++ tests/test_tuning_file.py
@@ -1,10 +1,11 @@
 from scicamera import Camera, CameraConfig
-from scicamera.tuning import find_tuning_algo, load_tuning_file
 from scicamera.testing import requires_camera_model
+from scicamera.tuning import find_tuning_algo, load_tuning_file
 
 # Here we load up the tuning for the HQ cam and alter the default exposure profile.
 # For more information on what can be changed, see chapter 5 in
 # https://datasheets.raspberrypi.com/camera/raspberry-pi-camera-guide.pdf
+
 
 def test_tuning_file():
     tuning = load_tuning_file("imx477.json")
