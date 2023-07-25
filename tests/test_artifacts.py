a5f18983dabd3860657c71d7acf6c26da210085f
meawoppl <meawoppl@gmail.com>
2023-07-25 11:28:31 -0700

    Add the test code

---
+++ tests/test_artifacts.py
@@ -0,0 +1,36 @@
+import os
+from typing import Type
+
+import pytest
+
+from scicamera import Camera, CameraConfig, FakeCamera
+from scicamera.testing import mature_after_frames_or_timeout
+
+import numpy as np
+
+
+# This test is a bit unusual. It captures the raw arrays and
+# exports them as npy files. These files are then loaded and
+# archived into githubs artifacts system. This allows to visually
+# check the raw arrays for correctness.
+
+@pytest.mark.parametrize("CameraClass", [Camera, FakeCamera])
+def test_capture_file_encodings(CameraClass: Type[Camera]):
+    if not os.path.isdir("artifacts"):
+        os.mkdir("artifacts")
+
+    with CameraClass() as camera:
+        preview_config = CameraConfig.for_still(camera, main={})
+        preview_config.enable_raw()
+        camera.configure(preview_config)
+
+        camera.start()
+
+        for stream_name in ("main", "raw"):
+            mature_after_frames_or_timeout(camera)
+            filepath = f"artifacts/{stream_name}-0.png"
+            array = camera.capture_array(name=stream_name).result()
+            np.save(filepath, array)
+            assert os.path.isfile(filepath)
+
+        camera.stop()
